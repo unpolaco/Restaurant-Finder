733cddce3ed988fa36bff89e13d9227b73eebb63
Michal Borowski <unpolacoo@gmail.com>
2020-01-16 14:04:27 +0100

    Mapping through fetch data

--- src/App.js
+++ src/App.js
@@ -11,42 +11,62 @@ import Form from './components/Form'
 export default class App extends Component {
 state = {
   inputValue: "",
-
+  venues: {
   name: "",
   adress: "",
   category: "",
-
+},
 
   restaurantsData: [],
   isLoaded: false,
-  restaurantNames: null ,
-}
+  restaurants: [
+    {name:'',
+      address: '',
+      lat: '',
+      lng: '',
+
+  }
+  ] ,
+};
 
     
-componentDidMount() {
 
-}
 handleSubmit = (e) => {
   e.preventDefault();
-
+  console.log(this.state.inputValue);
   const fourSquareUrl= "https://api.foursquare.com/v2/venues/explore?"
   const parameters = {
   client_id: "IEV0NGQ2WLUULDQ1TA0OD1UPUKZG0VTO3MYIKDN2MYHIKJ1E",
   client_secret: "SVPZ5HDAZK0JUFDNNVQBAWODV0FNG25YGM1EL5MB4SCSKRWD",
   query: "food",
-  near: "Warsaw",
+  near: this.state.inputValue ,
   v: 20201101,
 }
+
 
     fetch(fourSquareUrl + new URLSearchParams(parameters))
     .then(res => res.json())
     .then(res => {
       console.log(res.response.groups[0].items);
+      console.log(fourSquareUrl + new URLSearchParams(parameters));
       
-      this.setState({
+      this.setState(prevState => ({
         restaurantsData: res.response.groups[0].items,
         isLoaded: true,
-      })
+        restaurants: res.response.groups[0].items.map(el => (
+            {name: el.venue.name,
+             address: {
+               ulica: el.venue.location.formattedAddress[0],
+               miasto:el.venue.location.formattedAddress[1],
+               kraj: el.venue.location.formattedAddress[3],
+              },
+              lat: el.venue.location.lat,
+              lng: el.venue.location.lng,  
+              category: el.venue.categories[0].name,
+
+  }
+   ))
+      }))
     })
     .catch(error => console.log("Błąd: ", error));
   
@@ -55,6 +75,7 @@ handleSubmit = (e) => {
   handleInputChange = (e) => {
     this.setState({
       inputValue: e.target.value,
+      
     })
   }
 
