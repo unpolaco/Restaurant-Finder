0ee196a49588fa8e2860cb5047c7070eb8062b9b
Michal Borowski <unpolacoo@gmail.com>
2020-04-18 01:52:19 +0200

    Updates

---
+++ precache-manifest.a21cf74b9c77c85e8e9c2bb96ba1045d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02f322ef832f536d42dba19c8f4ed9e",
+    "url": "/Restaurant-Finder/index.html"
+  },
+  {
+    "revision": "e1faf3a13308fe8d86e4",
+    "url": "/Restaurant-Finder/static/css/2.36f280e5.chunk.css"
+  },
+  {
+    "revision": "e1faf3a13308fe8d86e4",
+    "url": "/Restaurant-Finder/static/js/2.bf6e9de7.chunk.js"
+  },
+  {
+    "revision": "ce3126c332c3bc8f1544c88c174f7c51",
+    "url": "/Restaurant-Finder/static/js/2.bf6e9de7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e76a6b5dd1409006945f",
+    "url": "/Restaurant-Finder/static/js/main.2e4f2492.chunk.js"
+  },
+  {
+    "revision": "6412871b811a837ebd70",
+    "url": "/Restaurant-Finder/static/js/runtime-main.49d91ff6.js"
+  },
+  {
+    "revision": "339bba5d30bf66d61d41c54020f82e0e",
+    "url": "/Restaurant-Finder/static/media/map-pointer.339bba5d.svg"
+  }
+]);
