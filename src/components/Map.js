16a80227d779a618ff737c2a25e93f2d9249ea91
Michal Borowski <unpolacoo@gmail.com>
2020-07-30 13:24:56 +0200

    Change Map to functional component with useRef and useEffect hooks, change marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,71 +1,54 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import icon from '../assets/svg/map-pointer.svg';
+import {
+	Map as LeafletMap,
+	TileLayer,
+	Marker,
+	Tooltip,
+} from 'react-leaflet';
+import restaurantMarker from '../assets/img/restaurant-marker.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
-export default class Map extends React.Component {
+function Map({ cityCenterPosition, onSelectMarker, restaurantData }) {
+	const mapRef = useRef();
+	useEffect(() => {
+		const { current = {} } = mapRef;
+		const { leafletElement: map } = current;
+		map.flyTo(cityCenterPosition, 12, { duration: 3 });
+	}, [cityCenterPosition]);
 
-	handleClickMarker = (e) => {
-		// console.log(e.target.options.id)
-	}
-	componentDidMount() {
-		this.map = L.map('map', {
-			center: this.props.cityCenterPosition,
-			zoom: 12,
-			zoomControl: false,
-		});
-		L.tileLayer(
-			'https://api.mapbox.com/styles/v1/unpolaco/ck5d0k8m403ju1iqo74h3bii4/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidW5wb2xhY28iLCJhIjoiY2swNW9qc3ZtMzB1cTNtcGswdnZkdW82cSJ9.3O4zCHkCvu4zhkQ_dZ1Vvg',
-			{
-				detectRetina: true,
-				maxZoom: 20,
-				maxNativeZoom: 17,
-			}
-		).addTo(this.map);
-		this.layer = L.layerGroup().addTo(this.map);
-
-		let DefaultIcon = L.icon({
-			iconUrl: icon,
-			iconSize: [27, 36],
-		});
-		L.Marker.prototype.options.icon = DefaultIcon;
-	}
-
-	componentDidUpdate({ restaurantData, selectedRestaurant }) {
-		if (this.props.restaurantData !== restaurantData) {
-			this.updateMarkers(this.props.restaurantData);
-		}
-		this.map.flyTo(this.props.cityCenterPosition, 12);
-	}
-	updateMarkers(restaurantData) {
-		this.layer.clearLayers();
-		restaurantData.forEach((marker) => {
-			L.marker(marker.latLng, { title: marker.name, id: marker.id })
-				.addTo(this.layer)
-				.on('click', this.handleClickMarker)
-				.bindPopup(marker.name)
-        .on('mouseover', function (e) {
-            this.openPopup();
-        })
-        .on('mouseout', function (e) {
-            this.closePopup();
-        })
-		});
-	}
-	render() {
-		const { cityCenterPosition, onSelectMarker } = this.props;
-		return (
-			<MapWrapper
-				id='map'
-				cityCenterPosition={cityCenterPosition}
-				onClick={onSelectMarker}
-			></MapWrapper>
-		);
-	}
+	const restaurantIcon = new L.Icon({
+		iconUrl: restaurantMarker,
+		iconRetinaUrl: restaurantMarker,
+		iconSize: [27, 36],
+		shadowUrl: markerShadow,
+		shadowAnchor: [13, 22],
+		tooltipAnchor: [15, -10],
+	});
+	return (
+		<MapWrapper
+			ref={mapRef}
+			center={cityCenterPosition}
+			zoom={12}
+			zoomControl={false}
+		>
+			<TileLayer url='https://api.mapbox.com/styles/v1/unpolaco/ck5d0k8m403ju1iqo74h3bii4/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidW5wb2xhY28iLCJhIjoiY2swNW9qc3ZtMzB1cTNtcGswdnZkdW82cSJ9.3O4zCHkCvu4zhkQ_dZ1Vvg' />
+			{restaurantData.map((marker) => (
+				<Marker
+					key={marker.id}
+					onClick={onSelectMarker}
+					icon={restaurantIcon}
+					position={(marker.lat, marker.latLng)}
+				>
+					<Tooltip>{marker.name}</Tooltip>
+				</Marker>
+			))}
+		</MapWrapper>
+	);
 }
-
-const MapWrapper = styled.div`
+const MapWrapper = styled(LeafletMap)`
 	position: fixed;
 	float: right;
 	width: 700px;
@@ -76,14 +59,15 @@ const MapWrapper = styled.div`
 	border-radius: 350px;
 	z-index: 0;
 	@media (min-width: 1024px) {
-    position: absolute;
+		position: absolute;
 		top: 150px;
 		left: 600px;
-  }
+	}
 	@media (max-width: 500px) {
-    position: fixed;
+		position: fixed;
 		bottom: -100px;
 		width: 90vw;
 		height: 90vw;
-  }
+	}
 `;
+export default Map;
