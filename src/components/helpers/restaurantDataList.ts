e249fa3b2e92968393006d76d60001ac6f5b0bb8
Michal Borowski <unpolacoo@gmail.com>
2022-12-23 13:15:17 +0100

    Add hardcoded fsq responses

---
+++ src/components/helpers/restaurantDataList.ts
@@ -0,0 +1,131 @@
+export const restaurantsListHardcoded = [
+    {
+        "name": "Teatro Cubano Warsaw",
+        "restaurantId": "58c9abc5c30f79556f47163e",
+        "timezone": "Europe/Warsaw",
+        "link": "/v3/places/58c9abc5c30f79556f47163e",
+        "location": {
+            "country": "PL",
+            "crossStreet": "Wierzbowa",
+            "formattedAddress": "ul. Aleksandra Fredry 6, Warsaw (Wierzbowa), 00-097 Warszawa",
+            "locality": "Warsaw",
+            "postcode": "00-097",
+            "region": "Mazowieckie",
+            "address": "ul. Aleksandra Fredry 6, Warsaw"
+        },
+        "categories": [
+            {
+                "categoryName": "Night Club",
+                "categoryId": 10032,
+                "categoryIcon": "https://ss3.4sqi.net/img/categories_v2/nightlife/nightclub_.png"
+            },
+            {
+                "categoryName": "Bar",
+                "categoryId": 13003,
+                "categoryIcon": "https://ss3.4sqi.net/img/categories_v2/nightlife/pub_.png"
+            },
+            {
+                "categoryName": "Cuban Restaurant",
+                "categoryId": 13135,
+                "categoryIcon": "https://ss3.4sqi.net/img/categories_v2/food/cuban_.png"
+            }
+        ],
+        "geocodes": {
+            "main": {
+                "latitude": 52.242226,
+                "longitude": 21.009645
+            },
+            "roof": {
+                "latitude": 52.242226,
+                "longitude": 21.009645
+            }
+        }
+    },
+    {
+        "name": "Buena Vista Social Food",
+        "restaurantId": "5af2c8ec492814002c6341d4",
+        "timezone": "Europe/Warsaw",
+        "link": "/v3/places/5af2c8ec492814002c6341d4",
+        "location": {
+            "country": "PL",
+            "crossStreet": "",
+            "formattedAddress": "00-667 Warszawa",
+            "locality": "Warsaw",
+            "postcode": "00-667",
+            "region": "Mazowieckie"
+        },
+        "categories": [
+            {
+                "categoryName": "Cuban Restaurant",
+                "categoryId": 13135,
+                "categoryIcon": "https://ss3.4sqi.net/img/categories_v2/food/cuban_.png"
+            }
+        ],
+        "geocodes": {
+            "main": {
+                "latitude": 52.222474,
+                "longitude": 21.010752
+            },
+            "roof": {
+                "latitude": 52.222474,
+                "longitude": 21.010752
+            }
+        }
+    },
+    {
+        "name": "Buena Vista Social Food",
+        "restaurantId": "629126099483142c8af1cb5f",
+        "timezone": "Europe/Warsaw",
+        "link": "/v3/places/629126099483142c8af1cb5f",
+        "location": {
+            "country": "PL",
+            "crossStreet": "",
+            "formattedAddress": "00-841 Warszawa",
+            "locality": "Warsaw",
+            "postcode": "00-841",
+            "region": "Mazowieckie"
+        },
+        "categories": [
+            {
+                "categoryName": "Cuban Restaurant",
+                "categoryId": 13135,
+                "categoryIcon": "https://ss3.4sqi.net/img/categories_v2/food/cuban_.png"
+            }
+        ],
+        "geocodes": {
+            "main": {
+                "latitude": 52.232204,
+                "longitude": 20.990473
+            }
+        }
+    },
+    {
+        "name": "Buena Vista Social Food",
+        "restaurantId": "5d31ab2ac30a9b0008a04d85",
+        "timezone": "Europe/Warsaw",
+        "link": "/v3/places/5d31ab2ac30a9b0008a04d85",
+        "location": {
+            "country": "PL",
+            "crossStreet": "",
+            "formattedAddress": "01-943",
+            "postcode": "01-943"
+        },
+        "categories": [
+            {
+                "categoryName": "Cuban Restaurant",
+                "categoryId": 13135,
+                "categoryIcon": "https://ss3.4sqi.net/img/categories_v2/food/cuban_.png"
+            }
+        ],
+        "geocodes": {
+            "main": {
+                "latitude": 52.296018,
+                "longitude": 20.931381
+            },
+            "roof": {
+                "latitude": 52.296018,
+                "longitude": 20.931381
+            }
+        }
+    }
+]
